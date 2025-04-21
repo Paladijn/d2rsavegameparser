dd2cd02573af77a0702f2684e615d82795a52797
Paladijn <119168865+Paladijn@users.noreply.github.com>
2025-04-21 14:39:05 +0200

    functionality: added the translation itemSkillTabKey to the SkillTree enum

--- src/main/java/io/github/paladijn/d2rsavegameparser/model/SkillTree.java
+++ src/main/java/io/github/paladijn/d2rsavegameparser/model/SkillTree.java
@@ -25,112 +25,112 @@ public enum SkillTree {
     /**
      * Skill tree for Bow and Crossbow skills (Amazon Only).
      */
-    BOW(0, CharacterType.AMAZON, "SkillCategoryAm3"),
+    BOW(0, CharacterType.AMAZON, "SkillCategoryAm3", "StrSklTabItem3"),
 
     /**
      * Skill tree for Passive and Magic skills (Amazon Only).
      */
-    PASSIVE(1, CharacterType.AMAZON, "SkillCategoryAm2"),
+    PASSIVE(1, CharacterType.AMAZON, "SkillCategoryAm2", "StrSklTabItem2"),
 
     /**
      * Skill tree for Javelin and Spear skills (Amazon Only).
      */
-    JAVELIN(2, CharacterType.AMAZON, "SkillCategoryAm1"),
+    JAVELIN(2, CharacterType.AMAZON, "SkillCategoryAm1", "StrSklTabItem1"),
 
     /**
      * Skill tree for Fire skills (Sorceress Only).
      */
-    FIRE(8, CharacterType.SORCERESS, "SkillCategorySo3"),
+    FIRE(8, CharacterType.SORCERESS, "SkillCategorySo3", "StrSklTabItem15"),
 
     /**
      * Skill tree for Lightning skills (Sorceress Only).
      */
-    LIGHTNING(9, CharacterType.SORCERESS, "SkillCategorySo2"),
+    LIGHTNING(9, CharacterType.SORCERESS, "SkillCategorySo2", "StrSklTabItem14"),
 
     /**
      * Skill tree for Cold skills (Sorceress Only).
      */
-    COLD(10, CharacterType.SORCERESS, "SkillCategorySo1"),
+    COLD(10, CharacterType.SORCERESS, "SkillCategorySo1", "StrSklTabItem13"),
 
     /**
      * Skill tree for Curses (Necromancer Only).
      */
-    CURSES(16, CharacterType.NECROMANCER, "SkillCategoryNe3"),
+    CURSES(16, CharacterType.NECROMANCER, "SkillCategoryNe3", "StrSklTabItem8"),
 
     /**
      * Skill tree for Poison and Bone skills (Necromancer Only).
      */
-    POISON_BONE(17, CharacterType.NECROMANCER, "SkillCategoryNe2"),
+    POISON_BONE(17, CharacterType.NECROMANCER, "SkillCategoryNe2", "StrSklTabItem7"),
 
     /**
      * Skill tree for Summoning skills (Necromancer Only).
      */
-    SUMMON(18, CharacterType.NECROMANCER, "SkillCategoryNe1"),
+    SUMMON(18, CharacterType.NECROMANCER, "SkillCategoryNe1", "StrSklTabItem16"),
 
     /**
      * Skill tree for Combat skills (Paladin Only).
      */
-    COMBAT(24, CharacterType.PALADIN, "SkillCategoryPa3"),
+    COMBAT(24, CharacterType.PALADIN, "SkillCategoryPa3", "StrSklTabItem6"),
 
     /**
      * Skill tree for Offensive Aura skills (Paladin Only).
      */
-    OFFENSIVE(25, CharacterType.PALADIN, "SkillCategoryPa2"),
+    OFFENSIVE(25, CharacterType.PALADIN, "SkillCategoryPa2", "StrSklTabItem5"),
 
     /**
      * Skill tree for Defensive Aura skills (Paladin Only).
      */
-    DEFENSIVE(26, CharacterType.PALADIN, "SkillCategoryPa1"),
+    DEFENSIVE(26, CharacterType.PALADIN, "SkillCategoryPa1", "StrSklTabItem4"),
 
     /**
      * Skill tree for Combat skills (Barbarian Only).
      */
-    COMBAT_BARB(32, CharacterType.BARBARIAN, "SkillCategoryBa3"),
+    COMBAT_BARB(32, CharacterType.BARBARIAN, "SkillCategoryBa3", "StrSklTabItem11"),
 
     /**
      * Skill tree for Masteries skills (Barbarian Only).
      */
-    MASTERIES(33, CharacterType.BARBARIAN, "SkillCategoryBa2"),
+    MASTERIES(33, CharacterType.BARBARIAN, "SkillCategoryBa2", "StrSklTabItem12"),
 
     /**
      * Skill tree for Warcry skills (Barbarian Only).
      */
-    SHOUT(34, CharacterType.BARBARIAN, "SkillCategoryBa1"),
+    SHOUT(34, CharacterType.BARBARIAN, "SkillCategoryBa1", "StrSklTabItem10"),
 
     /**
      * Skill tree for Summoning skills (Druid Only).
      */
-    SUMMON_DRUID(40, CharacterType.DRUID, "SkillCategoryDr3"),
+    SUMMON_DRUID(40, CharacterType.DRUID, "SkillCategoryDr3", "StrSklTabItem16"),
 
     /**
      * Skill tree for Shape-Shifting skills (Druid Only).
      */
-    SHAPESHIFT(41, CharacterType.DRUID, "SkillCategoryDr2"),
+    SHAPESHIFT(41, CharacterType.DRUID, "SkillCategoryDr2", "StrSklTabItem17"),
 
     /**
      * Skill tree for Elemental skills (Druid Only).
      */
-    ELEMENTAL(42, CharacterType.DRUID, "SkillCategoryDr1"),
+    ELEMENTAL(42, CharacterType.DRUID, "SkillCategoryDr1", "StrSklTabItem18"),
 
     /**
      * Skill tree for Trap skills (Assassin Only).
      */
-    TRAPS(48, CharacterType.ASSASSIN, "SkillCategoryAs3"),
+    TRAPS(48, CharacterType.ASSASSIN, "SkillCategoryAs3", "StrSklTabItem19"),
 
     /**
      * Skill tree for Shadow Discipline skills (Assassin Only).
      */
-    SHADOW(49, CharacterType.ASSASSIN, "SkillCategoryAs2"),
+    SHADOW(49, CharacterType.ASSASSIN, "SkillCategoryAs2", "StrSklTabItem20"),
 
     /**
      * Skill tree for Martial Art skills (Assassin Only).
      */
-    MARTIAL(50, CharacterType.ASSASSIN, "SkillCategoryAs1"),
+    MARTIAL(50, CharacterType.ASSASSIN, "SkillCategoryAs1", "StrSklTabItem21"),
 
     /**
      * Unknown skill tree.
      */
-    UNKNOWN(999, CharacterType.NONE, "Unknown skilltree (or not linked to any character type)");
+    UNKNOWN(999, CharacterType.NONE, "Unknown skilltree (or not linked to any character type)", "unknown skill tab");
 
     private final int index;
 
@@ -138,10 +138,13 @@ public enum SkillTree {
 
     private final String descriptionKey;
 
-    SkillTree(int index, CharacterType characterType, String descriptionKey) {
+    private final String itemSkillTabKey;
+
+    SkillTree(int index, CharacterType characterType, String descriptionKey, String itemSkillTabKey) {
         this.index = index;
         this.characterType = characterType;
         this.descriptionKey = descriptionKey;
+        this.itemSkillTabKey = itemSkillTabKey;
     }
 
     /**
@@ -179,9 +182,18 @@ public enum SkillTree {
     /**
      * Gets the description of the skill tree.
      *
-     * @return The description of the skill tree.
+     * @return The translatin key for the skill tree.
      */
     public final String getDescriptionKey() {
         return descriptionKey;
+    }
+
+    /**
+     * Gets the translation key for the skill tab, StrSklTabItem from item-modifiers.json
+     *
+     * @return the translation key for the skill tab
+     */
+    public final String getItemSkillTabKey() {
+        return itemSkillTabKey;
     }
 }
